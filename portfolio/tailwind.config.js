a5c643d044397cdaef99ee93be501e68a091bd7e
dev-riswa05 <warisouattara28@gmail.com>
2026-03-07 12:25:50 +0100

    mes modifications locales

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -8,4 +8,22 @@ export default {
     extend: {},
   },
   plugins: [],
+}   
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#0d6cf2",
+        "background-dark": "#0a0a0e",
+      },
+      fontFamily: {
+        "display": ["Space Grotesk", "sans-serif"]
+      },
+    },
+  },
+  plugins: [],
 }
